737369b5efb9929e4b73d917563f7b4ec633c6a0
nastre3 <nastre70@gmail.com>
2021-10-15 16:51:47 +1000

    Added lightUp method, 20.1.3, 20.1.6 tasks

--- Module1/src/Decoder.java
+++ Module1/src/Decoder.java
@@ -1,10 +1,54 @@
 public class Decoder {
     public static void main(String[] args) {
+        //Тернарный оператор
+        int variable = 7;
+        int res = variable > 10 ? variable - 10 : variable + 10;
+        System.out.println(res);
+
+
         char ch1 = 'h';
         char ch2 = 'i';
         char ch3 = 'b';
+        String trafficColor = "green";
 
         System.out.println(decode(ch1) + " " + decode(ch2) + " " + decode(ch3));
+        System.out.println(lightUp(trafficColor));
+        //Задача 20.1.3
+        int i = 10;
+        do {i--;}
+        while (i>1);
+        System.out.println("Value i = " + i);
+
+        //Задача 20.1.6
+        //Java 12
+        String trafficLight = "red";
+        String result = switch (trafficLight) {
+            case "red" -> "stop";
+            case "green" -> "go";
+            case "orange" -> "wait";
+            default -> "broken";
+        };
+        System.out.println(result);
+
+        if (trafficLight.equals("red")) {
+            System.out.println("stop");
+        } else if (trafficLight.equals("green")) {
+            System.out.println("go");
+        } else if (trafficLight.equals("orange")) {
+            System.out.println("wait");
+        } else {
+            System.out.println("broken");
+        }
+    }
+
+    private static String lightUp(String trafficColor) {
+        String result = switch (trafficColor) {
+            case "red" -> "stop";
+            case "green" -> "go";
+            case "orange" -> "wait";
+            default -> "broken";
+        };
+        return result;
     }
 
     //Java 13
